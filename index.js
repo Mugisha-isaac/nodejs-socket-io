6c468f5245b8377687b53b157c3ec761526d2020
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-04 10:57:46 +0200

    added ReciveFromClient and SendToClient events

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+var io = require('socket.io')(http);
+// console.log(io);
+io.on('connection',(socket)=>{
+    console.log('client connection received');
+socket.emit('SendToClient: ',{hello:'hello world'});
+
+socket.on('ReceivedFromClient', (data)=>{
+    console.log(data);
+})
+
+})
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+// io.on('connection',(socket)=>{
+//     console.log('client connection received');
+// })
+
+http.listen(3000,()=>{
+    console.log('server is running');
+})
